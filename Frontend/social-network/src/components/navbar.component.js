9fffec0b01c40ebe7cb3b2825e80d42877cafde4
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-04-05 20:10:38 +0100

    update nav bar

--- Frontend/social-network/src/components/navbar.component.js
+++ Frontend/social-network/src/components/navbar.component.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import "./style.css";
 
 export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <nav className="navbar navbar-dark bg-dark ">
         <div class="col-10">
         <Link to="/" className="navbar-brand">Social netwok application</Link>
         </div>
@@ -14,7 +30,25 @@ export default class Navbar extends Component {
         <div className="collpase navbar-collapse">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
-                <Link to="/create" className="nav-link">Profil</Link>
+              <MenuItem>
+              <Link to="/chat" className="nav-link">
+                    <IconButton aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="secondary">
+                         <MailIcon />
+                      </Badge>
+                   </IconButton>
+                </Link>
+                <Link to="/myprofil" className="nav-link">
+                <IconButton aria-label="account of current user" aria-controls="primary-search-account-menu" aria-haspopup="true"  color="inherit"  >
+                     <ExitToAppIcon />
+                   </IconButton>
+                </Link>
+                <Link to="/myprofil" className="nav-link">
+                <IconButton aria-label="account of current user" aria-controls="primary-search-account-menu" aria-haspopup="true"  color="inherit"  >
+                        <AccountCircle />
+                   </IconButton>
+                </Link>
+                </MenuItem>
               </li>
             </ul>
           </div>
---
+++ Frontend/social-network/src/pages/profil.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+export default class Profil extends Component {
+
+    render() {
+      return (
+        <div
+        style={{
+          display: 'flex',
+          justifyContent: 'Right',
+          alignItems: 'Right',
+          height: '100vh'
+        }}
+      >
+        <h1> Profil page </h1>
+      </div>
+      );
+    }
+  }
