d9fa59ed777a75af1c68d68bddfd3442cc03a657
lkhadir <lkhadir.mostapha@gmail.com>
2021-04-06 14:54:58 +0100

    projet structure

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const api = require('./routes/api')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+// set up express app
+const app = express();
+//use body parser middleware
+//app.use(bodyParser.json());
+//initialize routes
+app.use(api);
+//listen for requests
+app.listen(process.env.port || 3000, function(){
+  console.log('now listening for requests');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+ 
