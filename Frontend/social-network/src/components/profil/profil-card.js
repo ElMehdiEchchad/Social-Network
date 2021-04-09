8890eab8d01b3a5015b7a65bd8456ec6bd5aac39
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-04-09 10:34:43 +0100

    Update profil-card.js

--- Frontend/social-network/src/components/profil/profil-card.js
+++ Frontend/social-network/src/components/profil/profil-card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import Divider from '@material-ui/core/Divider';
 import Ava from '@material-ui/core/Avatar';
@@ -12,40 +12,44 @@ import Avatar from "../profil/ava1.png";
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 
-const   StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: '$ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.9)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(4)',
-      opacity: 0,
-    },
-  },
-}))(Badge);
+export default function Profilcard() {
+   
+  const [value, setValue] = useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-
-export default class Profilcard extends Component {
+  const   StyledBadge = withStyles((theme) => ({
+    badge: {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: '$ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.9)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(4)',
+        opacity: 0,
+      },
+    },
+  }))(Badge);
   
-    render() {
+ 
       return (
         <div >
          <Card style={{ width: '70%'  , height : '40%' , position: 'absolute', left: '50%', top: '50%',
@@ -63,11 +67,17 @@ export default class Profilcard extends Component {
             <Divider/>
 
             <Paper square>
-              <Tabs indicatorColor="primary"  textColor="primary"aria-label="disabled tabs example">
-               <Tab label="About" />
-               <Tab label="Posts" />
-                <Tab label="Friends" />
-              </Tabs>
+                <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+                 >
+                     <Tab label="About me" />
+                     <Tab label="My friends" />
+                     <Tab label="Posts" />
+                </Tabs>
             </Paper>
             <br/>
             <div>
@@ -78,4 +88,6 @@ export default class Profilcard extends Component {
       </div>
       );
     }
-  }
+  
+
+ 
