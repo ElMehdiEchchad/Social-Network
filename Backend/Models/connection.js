79bc0f0ddbc77b43501876fdfc8827f3a57339f6
lkhadir <lkhadir.mostapha@gmail.com>
2021-04-27 15:44:25 +0000

    implementation of CORS prevention

--- Backend/Models/connection.js
+++ Backend/Models/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const URI = "mongodb+srv://root:"+process.env.MONGO_ATLAS_PW+"@cluster0.ao4lo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectDB = async () => {
-    await mongoose.connect(URI,{ useNewUrlParser: true ,useUnifiedTopology: true})
+    await mongoose.connect(URI,{ useCreateIndex: true,useNewUrlParser: true ,useUnifiedTopology: true})
     console.log("database has been connected");
 }
 
--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const connectDB =  require ('./Models/connection');
-const AuthentificationRoutes = require ('./Routes/API/Authentification');
+const connectDB = require('./Models/connection');
+const AuthentificationRoutes = require('./Routes/API/Authentification');
 const UserRoutes = require('./Routes/API/User');
 
 
@@ -11,14 +11,24 @@ const UserRoutes = require('./Routes/API/User');
 const app = express();
 //use connection to mongodb server
 connectDB();
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
+//Adding CORS 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT, POST , PATCH , DELETE , GET');
+    return res.status(200).json({})
+  }
+  next(); 
+}) 
 //initialize routes of API
 app.use(UserRoutes);
-app.use('/api/users/',AuthentificationRoutes);
+app.use('/api/users/', AuthentificationRoutes);
 
 //listen for requests
-app.listen(process.env.port || 3000, function(){
+app.listen(process.env.port || 3000, function () {
   console.log('now listening for requests');
 });
- 
+
