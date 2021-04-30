f507d33001bc7ff837085832de489c528f2e5bbe
lkhadir <lkhadir.mostapha@gmail.com>
2021-04-30 01:41:48 +0000

    change PORT to 5000

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const connectDB = require('./Models/connection');
 const AuthentificationRoutes = require('./Routes/API/Authentification');
 const UserRoutes = require('./Routes/API/User');
+const io = require('socket.io');
 
 
 // set up express app
@@ -16,19 +17,67 @@ app.use(express.json({ extended: false }));
 //Adding CORS 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-Methods','PUT, POST , PATCH , DELETE , GET');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST , PATCH , DELETE , GET');
     return res.status(200).json({})
   }
-  next(); 
-}) 
+  next();
+})
 //initialize routes of API
 app.use(UserRoutes);
-app.use('/api/users/', AuthentificationRoutes);
+app.use('/api/', AuthentificationRoutes);
 
 //listen for requests
-app.listen(process.env.port || 3000, function () {
+app.listen(process.env.port || 5000, function () {
   console.log('now listening for requests');
 });
 
+
+// const io = socketIo(app);
+// io.on("connection", socket => {
+//   //  console.log("New client connected" + socket.id);
+//   //console.log(socket);
+//   // Returning the initial data of food menu from FoodItems collection
+//   socket.on("initial_data", () => {
+//     collection_foodItems.find({}).then(docs => {
+//       io.sockets.emit("get_data", docs);
+//     });
+//   });
+//   // Placing the order, gets called from /src/main/PlaceOrder.js of Frontend
+//   socket.on("putOrder", order => {
+//     collection_foodItems
+//       .update({ _id: order._id }, { $inc: { ordQty: order.order } })
+//       .then(updatedDoc => {
+//         // Emitting event to update the Kitchen opened across the devices with the realtime order values
+//         io.sockets.emit("change_data");
+//       });
+//   });
+//   // Order completion, gets called from /src/main/Kitchen.js
+//   socket.on("mark_done", id => {
+//     collection_foodItems
+//       .update({ _id: id }, { $inc: { ordQty: -1, prodQty: 1 } })
+//       .then(updatedDoc => {
+//         //Updating the different Kitchen area with the current Status.
+//         io.sockets.emit("change_data");
+//       });
+//   });
+
+//   // Functionality to change the predicted quantity value, called from /src/main/UpdatePredicted.js
+//   socket.on("ChangePred", predicted_data => {
+//     collection_foodItems
+//       .update(
+//         { _id: predicted_data._id },
+//         { $set: { predQty: predicted_data.predQty } }
+//       )
+//       .then(updatedDoc => {
+//         // Socket event to update the Predicted quantity across the Kitchen
+//         io.sockets.emit("change_data");
+//       });
+//   });
+
+//   // disconnect is fired when a client leaves the server
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
