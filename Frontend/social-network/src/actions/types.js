457f4ee46c107cdc98c51e6e0c3ec7412a8fd612
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-05-07 14:48:56 +0000

    fix ---

--- Frontend/social-network/src/actions/types.js
+++ Frontend/social-network/src/actions/types.js
@@ -1,10 +1,7 @@
 export const GET_USER = 'GET_USER';
 export const ADD_USER = 'ADD_ITEM';
-
-export const UPDATE_USER = 'UPDATE_ITEM';
 export const ITEMS_LOADING ='ITEMS_LOADING';
 export const UPDATE_USER = 'UPDATE_USER';
-export const ITEMS_LOADING ='ITEMS_LOADING';
 export const GET_FREINDS  ='GET_FREINDS ';
 export const GET_ALLUSERS ='GET_ALLUSERS' ;
 
