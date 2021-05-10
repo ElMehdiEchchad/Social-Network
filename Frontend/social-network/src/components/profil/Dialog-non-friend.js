d02a0a1cb3585f299327c0446c0236cfbd61ed40
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-05-10 23:19:53 +0000

    remove posts from non friend profil

---
+++ Frontend/social-network/src/components/profil/Dialog-non-friend.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from "../profil/ava2.jpg";
+import Ava from '@material-ui/core/Avatar';
+import { Button} from 'semantic-ui-react'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+import ProfilcardNonFriend from "../profil/Profil-card-nonfriend";
+
+export default function ScrollDialog2(props) {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen('paper')}  style={{ color: "white" , backgroundColor :"white"}}> <Ava  src={Avatar} style={{ height: '80px', width: '80px' }}/></Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Profil </DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <ProfilcardNonFriend id={props.id}></ProfilcardNonFriend>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
